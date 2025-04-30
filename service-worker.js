a08990f6b2978411954b3dc647eadbf46de5ccb7
Johnny Molyneux <johnny.molyneux@gmail.com>
2025-04-30 10:10:07 +0100

    farm

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,22 @@
 self.addEventListener('install', (e) => {
-    e.waitUntil(
-      caches.open('brewcalc-cache').then((cache) => {
-        return cache.addAll(['./index.html', './logo.jpg']);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      caches.match(e.request).then((response) => {
-        return response || fetch(e.request);
-      })
-    );
-  });
-  
+  e.waitUntil(
+    caches.open('brewcalc-cache').then((cache) => {
+      return cache.addAll([
+        './index.html',
+        './liquor-back.html',
+        './can-fill.html',
+        './logo.jpg',
+        './cow.wav',
+        './pig.wav'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
